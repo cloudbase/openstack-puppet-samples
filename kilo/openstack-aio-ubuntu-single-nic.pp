f0ff2f08a8c76f456a626c0711cadf4588918e7c
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2015-08-07 13:15:37 +0300

    Improves MySQL configuration tuning

--- kilo/openstack-aio-ubuntu-single-nic.pp
+++ kilo/openstack-aio-ubuntu-single-nic.pp
@@ -65,7 +65,11 @@ exec { 'apt-update':
 
 class { 'mysql::server':
   root_password    => $admin_password,
-  override_options => { 'mysqld' => { 'bind-address' => '0.0.0.0' } },
+  override_options => { 'mysqld' => { 'bind_address'           => '0.0.0.0',
+                                      # Not necessary starting from MySQL 5.5
+                                      'default_storage_engine' => 'InnoDB',
+                                      'max_connections'        => 1024,
+                                      'open_files_limit'       => -1 } },
   restart          => true,
 }
 
